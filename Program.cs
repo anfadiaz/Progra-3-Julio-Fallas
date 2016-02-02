3a618fee55d0dc6d478e85de816868079e0e911e
anfadiaz <anfadiaz@hotmail.com>
2016-02-01 18:54:24 -0600

    Test

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_Persona
+{
+    class Program
+    {
+        
+        public class Persona
+        {
+            public string name;
+            public Persona()
+            {
+                name = "Unknown";
+            }
+            public Persona(string nm)
+            {
+                name = nm;
+            }
+            public void SetName(string NewName)
+            {
+                name = NewName;
+            }
+        }
+
+        static void Main()
+        {
+            Persona persona1 = new Persona();
+            Console.WriteLine(persona1.name);
+
+            persona1.SetName("Julio Fallas Diaz");
+            Console.WriteLine(persona1.name);
+
+            Persona persona2 = new Persona("Bob Patiño");
+            Console.WriteLine(persona2.name);
+
+            Console.WriteLine("Prescione cualquier tecla para continuar.");
+            Console.ReadKey();
+        }
+
+        
+    }
+}
